35cdfcee046c98608ffd9cbf372d3b604bd28293
Kabul Totakhil <totakhilkamran7@gmail.com>
2025-05-25 00:48:39 -0700

    add a testCase for purple color

--- src/ColorPrinterTest.java
+++ src/ColorPrinterTest.java
@@ -40,13 +40,33 @@ void testPrintWithBlueColor(){
   //Act
   String message = "Test for the blue color";
 
-  colorPrinter.print(message);
+  colorPrinter.println(message);
 
-  String expected = ConsoleColor.BLUE + "Test for the blue color" + ConsoleColor.RESET;
+  String expected = ConsoleColor.BLUE + "Test for the blue color" + System.lineSeparator() + ConsoleColor.RESET;
 
   //Assert
   assertEquals(expected, outputStream.toString());
 
  
 }
+
+@Test
+  void testPrintWithPurpleColor(){
+    // Arrange
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    PrintStream printStream = new PrintStream(outputStream);
+    ColorPrinter colorPrinter = new ColorPrinter(printStream);
+
+    colorPrinter.setCurrentColor(ConsoleColor.PURPLE);
+
+    // Act
+    String message = "Test for the purple color";
+    colorPrinter.println(message);
+    
+    // Assert: Check output
+    String expected = ConsoleColor.PURPLE + "Test for the purple color" + System.lineSeparator() + ConsoleColor.RESET;
+    assertEquals(expected, outputStream.toString());
+
+  }
 }
