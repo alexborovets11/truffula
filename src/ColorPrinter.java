05941a5bdd5f9727c93437513d1fdc3e7dfb6a02
Kabul Totakhil <totakhilkamran7@gmail.com>
2025-05-25 00:36:30 -0700

    create a test for printWithBlueColor

--- src/ColorPrinter.java
+++ src/ColorPrinter.java
@@ -87,6 +87,7 @@ public class ColorPrinter {
    */
   public void print(String message, boolean reset) {
     // TODO: Implement this!
+    
     printStream.print(currentColor);
 
     printStream.print(message);
--- src/ColorPrinterTest.java
+++ src/ColorPrinterTest.java
@@ -26,4 +26,27 @@ class ColorPrinterTest {
     // Assert: Verify the printed output
     assertEquals(expectedOutput, outputStream.toString());
   }
+
+
+@Test
+void testPrintWithBlueColor(){
+  ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+  PrintStream printStream = new PrintStream(outputStream);
+  ColorPrinter colorPrinter = new ColorPrinter(printStream);
+
+  colorPrinter.setCurrentColor(ConsoleColor.BLUE);
+
+  //Act
+  String message = "Test for the blue color";
+
+  colorPrinter.print(message);
+
+  String expected = ConsoleColor.BLUE + "Test for the blue color" + ConsoleColor.RESET;
+
+  //Assert
+  assertEquals(expected, outputStream.toString());
+
+ 
+}
 }
